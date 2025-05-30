cdd31181e07a9523eb867dee4adf32145f74fb9b
mattm <mtt.mortensen527@gmail.com>
2025-05-30 10:19:49 -0600

    updating to be local based scripts since it does take a while to run them from onedrive.

--- ScriptLauncher/Form1.cs
+++ ScriptLauncher/Form1.cs
@@ -16,7 +16,7 @@ namespace ScriptLauncher
             psi.FileName = "powershell.exe";
 
             // 2. Script Path from OneDrive
-            psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"& 'C:\\Users\\mattm\\OneDrive - 4Life Research\\Documents\\Scripts\\CountUsers.ps1'\"";
+            psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"& 'C:\\Users\\mattm\\Documents\\Scripts\\WF\\CountUsers.ps1'\"";
 
             // 3. Quiet Execution (no black window pop up)
             psi.UseShellExecute = false;
@@ -47,6 +47,45 @@ namespace ScriptLauncher
                 MessageBox.Show($"Done!\n {output}");
             }
 
+        }
+
+        private void get_ADServers(object sender, EventArgs e)
+        {
+            // 1. Create process settings 
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "powershell.exe";
+
+            // 2. Script Path from OneDrive
+            psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"& 'C:\\Users\\mattm\\Documents\\Scripts\\WF\\Get_AD_Servers.ps1'\"";
+
+            // 3. Quiet Execution (no black window pop up)
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            // 4. Capture any output or errors
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            // 5. Run the process 
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+
+            // 6. Wait and capture the result
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            p.WaitForExit();
+
+            // 7. Show basic success or error message
+            if (!string.IsNullOrEmpty(error))
+            {
+                MessageBox.Show($"Error:\n {error}");
+
+            }
+            else
+            {
+                MessageBox.Show($"Done!\n {output}");
+            }
         }
     }
 }
