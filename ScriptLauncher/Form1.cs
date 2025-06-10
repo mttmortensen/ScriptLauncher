3ee50260ee502fc29f295d2dc3f478b3f4a18452
mattm <mtt.mortensen527@gmail.com>
2025-06-10 09:18:08 -0600

    refactored to have OutPut function be for both click events

--- ScriptLauncher/Form1.cs
+++ ScriptLauncher/Form1.cs
@@ -11,47 +11,12 @@ namespace ScriptLauncher
 
         private void get_EnabledADUSers(object sender, EventArgs e)
         {
-            // 1. Create process settings 
-            ProcessStartInfo psi = new ProcessStartInfo();
-            psi.FileName = "powershell.exe";
-
-            // 2. Script Path from OneDrive
-            psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"& 'C:\\Users\\mattm\\Documents\\Scripts\\WF\\CountUsers.ps1'\"";
-
-            // 3. Quiet Execution (no black window pop up)
-            psi.UseShellExecute = false;
-            psi.CreateNoWindow = true;
-
-            // 4. Capture any output or errors
-            psi.RedirectStandardOutput = true;
-            psi.RedirectStandardError = true;
-
-            // 5. Run the process 
-            Process p = new Process();
-            p.StartInfo = psi;
-            p.Start();
-
-            // 6. Wait and capture the result
-            string output = p.StandardOutput.ReadToEnd();
-            string error = p.StandardError.ReadToEnd();
-            p.WaitForExit();
-
-            // 7. Show basic success or error message
-            if (!string.IsNullOrEmpty(error))
-            {
-                MessageBox.Show($"Error:\n {error}");
-
-            }
-            else
-            {
-                MessageBox.Show($"Done!\n {output}");
-            }
-
+            OutPut("C:\\Users\\mattm\\Documents\\Scripts\\WF\\CountUsers.ps1");
         }
 
         private void get_ADServers(object sender, EventArgs e)
         {
-            
+            OutPut("C:\\Users\\mattm\\Documents\\Scripts\\WF\\Get_AD_Servers.ps1");
         }
 
         private void OutPut(string filePath) 
@@ -60,8 +25,8 @@ namespace ScriptLauncher
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.FileName = "powershell.exe";
 
-            // 2. Script Path from OneDrive
-            psi.Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"& 'C:\\Users\\mattm\\Documents\\Scripts\\WF\\Get_AD_Servers.ps1'\"";
+            // 2. Script Path from File Explorer
+            psi.Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"& '{filePath}'\"";
 
             // 3. Quiet Execution (no black window pop up)
             psi.UseShellExecute = false;
